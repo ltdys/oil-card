6991cd041128afb8f6fbd0c5ca531617e09382a8
jiangdaye <692466154@qq.com>
2019-04-14 12:31:21 +0800

    test

--- src/views/home/components/nav.vue
+++ src/views/home/components/nav.vue
@@ -13,7 +13,7 @@ export default {
     return {
       navList: [{
         img: 'static/images/icon/appointment.png',
-        text: '预约办卡1',
+        text: '预约办卡',
         path: '/service/appointment'
       }, {
         img: 'static/images/icon/binding.png',
