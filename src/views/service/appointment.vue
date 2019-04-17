0a9bf85248b7bc790daa69f61e145631e3ad9a32
ltdys <692466154@qq.com>
2019-04-17 10:45:55 +0800

    添加公共样式

--- src/views/service/appointment.vue
+++ src/views/service/appointment.vue
@@ -2,6 +2,44 @@
   <com-page class="appointment">
     <com-header title="预约办卡" is-back slot="header"></com-header>
     
+    <van-field
+      v-model="formData.cardType"
+      required
+      clearable
+      label="油卡类型">
+    </van-field>
+    <van-field
+      v-model="formData.express"
+      required
+      clearable
+      disabled
+      label="快递选择"
+      right-icon="arrow">
+    </van-field>
+    <van-field
+      v-model="formData.addressee"
+      required
+      clearable
+      label="收件人"
+      placeholder="输入收件人姓名">
+    </van-field>
+    <van-field
+      v-model="formData.phone"
+      required
+      clearable
+      label="手机号"
+      placeholder="输入收件人手机号">
+    </van-field>
+    <van-field
+      v-model="formData.address"
+      required
+      clearable
+      label="收货地址"
+      placeholder="输入收货地址">
+    </van-field>
+    <div class="submit_buttons">
+      <van-button type="primary">立即申请</van-button>
+    </div>
   </com-page>
 </template>
 
@@ -9,7 +47,13 @@
 export default {
   data() {
     return {
-      
+      formData: {
+        cardType: '中国石化',  //油卡类型
+        express: '顺丰', //快递
+        addressee: '',  //收件人
+        phone: '',  //手机号
+        address: '',  //收货地址
+      }
     };
   },
   created() {
