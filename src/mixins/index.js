22a95ce1f8a11ac1eb807413fab3891eda00f9cc
jiangdaye <692466154@qq.com>
2019-04-17 22:20:49 +0800

    添加公共js

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,7 +1,33 @@
 export const list_mixins = {
   data () {
     return {
-      
+      pageIndex:1,
+      pageSize:20,
+      resultList:[],
+      finished:false
+    }
+  },
+
+  methods: {
+    loadTop() {
+      return new Promise(r=>{
+        setTimeout(()=>r(),500)
+      }).then(()=>{
+        return this.init_data();
+      }).then((res)=>{
+        this.$refs.loadmore && this.$refs.loadmore.onTopLoaded();
+        return res
+      })
+    },
+    loadBottom() {
+      if (this.finished){
+        return
+      }
+      return this.list_data().then((res)=>{
+        this.resultList = this.resultList.concat(res)
+        this.$refs.loadmore && this.$refs.loadmore.onBottomLoaded();
+        return res
+      })
     }
   }
 }
