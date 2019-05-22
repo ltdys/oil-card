963d465a799247a5fe2cb23d995ad860815fb541
ltdys <692466154@qq.com>
2019-05-22 13:30:03 +0800

    首页轮播跳转更改

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,7 +2,8 @@
   <div class="page-home">
     <van-swipe :autoplay="3000" indicator-color="white" >
         <van-swipe-item v-for="(item, index) in images" :key="index">
-          <img :src="HEAD_IMAGE_PR + item.image" class="w-100 h-150 block"/>
+          <img :src="HEAD_IMAGE_PR + item.image" class="w-100 h-150 block"
+            @click="jumpDetail(item)"/>
         </van-swipe-item>
     </van-swipe>
     <div class="page-home-middle">
@@ -40,6 +41,12 @@
         let resData = await getHeadPicList(params)
         if (resData.status === 200 && resData.data.code === 1) {
           this.images = resData.data.data
+          console.log("this.images", this.images)
+        }
+      },
+      jumpDetail (item) {
+        if (item.url !== "") {
+          window.location.href = item.url;
         }
       }
     }
